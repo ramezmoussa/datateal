8820cf2639093ca2a0ba1942cf0173ff5df51b8b
Tommi Hirvonen <tommih_@hotmail.com>
2026-04-04 11:23:37 +0300

    renamed control plane project

--- src/app-host/DuckHouse.AppHost/AppHost.cs
+++ src/app-host/DuckHouse.AppHost/AppHost.cs
@@ -1,8 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var api = builder.AddProject<Projects.DuckHouse_ControlPlane_Api>("api");
+var controlPlane = builder.AddProject<Projects.DuckHouse_ControlPlane>("control-plane");
 
 builder.AddProject<Projects.DuckHouse_Ui>("ui")
-    .WithReference(api);
+    .WithReference(controlPlane);
 
 builder.Build().Run();
